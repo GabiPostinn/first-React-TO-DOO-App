7819f045c6fb4a78015acdcab2ea5f44eaf74f14
GabiPostin <begere2009@gmail.com>
2025-07-22 22:26:47 +0300

    Adding to TaskCard a KEY={...}

--- src/App.js
+++ src/App.js
@@ -7,26 +7,28 @@ function App() {
       id: "T-1",
       name: "Put Turbo On Car.",
       status: "TODO",
-      dueDate: new Date(2026, 1, 23), // Asta nu inteleg cu "new Date"
+      dueDate: new Date(2026, 1, 23),
     },
-    // prettier-ignore
-    { id: "T-2",
+
+    {
+      id: "T-2",
       name: "Clean Your Room.",
       status: "Completed",
-      dueDate: new Date(2025, 7, 15) 
+      dueDate: new Date(2025, 7, 15),
     },
-    // prettier-ignore
-    { id: "T-3",
+
+    {
+      id: "T-3",
       name: "Learn Js.",
       status: "In Progress",
-      dueDate: new Date(2026, 7, 15)
+      dueDate: new Date(2026, 7, 15),
     },
-    //prettier-ignore
 
-    { id: "T-4",
+    {
+      id: "T-4",
       name: "Upgrade Your Car.",
       status: "In Progress",
-      dueDate: new Date(2027, 2, 19)
+      dueDate: new Date(2027, 2, 19),
     },
     {
       id: "T-5",
@@ -35,33 +37,30 @@ function App() {
       dueDate: new Date(2025, 6, 11),
     },
     {
-      id: "T-5",
+      id: "T-6",
       name: "Get A Job On Front End Developer.",
       status: "TODO",
       dueDate: new Date(2025, 6, 11),
     },
     {
-      id: "T-5",
+      id: "T-7",
       name: "Learn How To Invest Your Money.",
       status: "In Progress",
       dueDate: new Date(2025, 6, 11),
     },
   ];
-  // prettier-ignore
+
   return (
     <div className="app-container">
-
-      {data.map((task) => ( // MAP metoda care trece prin fiecare element din DATA
-
-      <TaskCard
-      id={task.id}
-      name={task.name}
-      status={task.status}
-      dueDate={task.dueDate}
-      />
-      
+      {data.map((task) => (
+        <TaskCard
+          key={task.id}
+          id={task.id}
+          name={task.name}
+          status={task.status}
+          dueDate={task.dueDate}
+        />
       ))}
-
     </div>
   );
 }
